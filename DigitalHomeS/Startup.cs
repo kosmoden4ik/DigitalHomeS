a65a6302f83635307d57480655c1f53cb9ea5d07
kosmoden4ik <kosmoden4ik@gmail.com>
2019-08-25 14:38:14 +0300

    Add project files.

---
+++ DigitalHomeS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalHomeS.Startup))]
+namespace DigitalHomeS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
